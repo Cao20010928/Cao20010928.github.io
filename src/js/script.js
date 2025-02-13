b80c800ea77eceaaa8d3ad5196ed612d9d029c82
Ego <11413929+MyEgo@user.noreply.gitee.com>
2025-02-13 15:45:26 +0900

    first submit

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+import $ from 'jQuery';
+$( document ).ready(function() {
+
+    var envelope = $('#envelope');
+    var btn_open = $("#love");
+    var btn_reset = $("#love");
+
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+            .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+            .removeClass("open");
+    }
+
+});
